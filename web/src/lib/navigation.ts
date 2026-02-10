c09d88ba30549e03dd957c43aa7389fffec9cde4
James Scott-Brown <James.Scott-Brown@london.gov.uk>
2026-02-10 11:23:21 +0000

    Tidy up navigation.ts (#8)
    Removed filtering and mapping logic for navigation children.
    
    Co-authored-by: mikeldn <35931345+mikeldn@users.noreply.github.com>

--- web/src/lib/navigation.ts
+++ web/src/lib/navigation.ts
@@ -2,36 +2,8 @@ import { getAllDocs } from './utils.js';
 
 const allDocs = getAllDocs();
 
-const introduction = allDocs
-	.filter((doc) => doc.section === 'Getting Started')
-	.filter((doc) => doc.slugFull !== '/design-system/getting-started')
-	.map((doc) => ({
-		title: doc.navLabel,
-		id: `${doc.title.toLowerCase()}-getting-started`,
-		href: `/${doc.slug}`
-	}));
-
-const foundations = allDocs
-	.filter((doc) => doc.section === 'Foundations')
-	.filter((doc) => doc.slugFull !== '/design-system/foundations')
-	.map((doc) => ({
-		title: doc.navLabel,
-		id: `${doc.title.toLowerCase()}-foundations`,
-		href: `/${doc.slug}`
-	}));
-
-const applicationDesign = allDocs
-	.filter((doc) => doc.section === 'Application design')
-	.filter((doc) => doc.slugFull !== '/design-system/application-design')
-	.map((doc) => ({
-		title: doc.navLabel,
-		id: `${doc.title.toLowerCase()}-application-design`,
-		href: `/${doc.slug}`
-	}));
-
-//Non alphabetic sort index. N.B reverse order so non listed pages are appended
-const dvPageOrder = ['Charts', 'Colour', 'Accessibility', 'Getting started'];
-
+// Can do something like this to enumerate child entries for a menu entry
+/*
 const dataVisualisation = allDocs
 	.filter((doc) => doc.section === 'Data visualisation')
 	.filter((doc) => doc.slugFull !== '/design-system/data-visualisation')
@@ -41,90 +13,91 @@ const dataVisualisation = allDocs
 		href: `/${doc.slug}`
 	}))
 	.sort((a, b) => dvPageOrder.indexOf(b.title) - dvPageOrder.indexOf(a.title));
+*/
 
 export const navigation = [
 	{
 		title: 'Foreword',
 		id: 'foreword',
 		href: '/report/foreword',
-		children: introduction
+		children: []
 	},
 	{
 		title: 'London in Figures',
 		id: 'london-in-figures',
 		href: '/report/london-in-figures',
-		children: foundations
+		children: []
 	},
 	{
 		title: 'Key Stories for London',
 		id: 'key-stories-for-london',
 		href: '/report/key-stories-for-london',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Demography',
 		id: 'demography',
 		href: '/report/demography',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Economy',
 		id: 'economy',
 		href: '/report/economy',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Global city and culture',
 		id: 'global-city-and-culture',
 		href: '/report/global-city-and-culture',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Skills',
 		id: 'skills',
 		href: '/report/skills',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Social justice',
 		id: 'social-justice',
 		href: '/report/social-justice',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Housing',
 		id: 'housing',
 		href: '/report/housing',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Environment',
 		id: 'environment',
 		href: '/report/environment',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Crime',
 		id: 'crime',
 		href: '/report/crime',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Transport',
 		id: 'transport',
 		href: '/report/transport',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Children and young people',
 		id: 'children-and-young-people',
 		href: '/report/children-and-young-people',
-		children: applicationDesign
+		children: []
 	},
 	{
 		title: 'Health',
 		id: 'health',
 		href: '/report/health',
-		children: applicationDesign
+		children: []
 	},
 ];
